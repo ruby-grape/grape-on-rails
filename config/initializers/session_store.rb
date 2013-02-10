9eaa4fa35a5c5a7240b0c7ec592a53e32e4d0e83
dblock <dblock@dblock.org>
2013-02-10 10:11:34 -0500

    Fix: Ruby 1.8.7.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-GrapeOnRails::Application.config.session_store :cookie_store, key: '_grape-on-rails_session'
+GrapeOnRails::Application.config.session_store :cookie_store, :key => '_grape-on-rails_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

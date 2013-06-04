9ca26f4341b93a221d24cc9e2d2c11b3883878c2
dblock <dblock@dblock.org>
2013-06-04 09:17:50 -0400

    Upgraded to Rails 4.

--- Gemfile
+++ Gemfile
@@ -1,16 +1,13 @@
 source 'https://rubygems.org'
 
-gem 'rails', '~> 3.2.13'
+gem 'rails', '4.0.0.rc1'
+gem 'sass-rails', '~> 4.0.0.rc1'
+gem 'coffee-rails', '~> 4.0.0.rc1'
 gem 'sqlite3', '~> 1.3.7', :platforms => [ :ruby ]
 gem 'activerecord-jdbcsqlite3-adapter', "~> 1.2.9", :platforms => [ :jruby ]
 gem 'jquery-rails', '~> 3.0.0'
 gem 'grape', '~> 0.4.1'
-
-group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
-  gem 'uglifier', '>= 1.0.3'
-end
+gem 'uglifier', '>= 1.0.3'
 
 group :development, :test do
   gem "rspec", "~> 2.13.0"
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GrapeOnRails::Application.config.secret_token = '254eb8693c37442a6d10d6f311ed3ea43b604475cfdd235bcd860db9c76877178f677b8a759ad0047002ecad94c99467cabb21005197934bbedc735b8bdf27b0'
+GrapeOnRails::Application.config.secret_key_base = 'secret'

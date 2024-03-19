c7ad0d690889b4a144a723e372f7e9d66099f38a
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2024-03-19 12:08:44 -0400

    Upgraded Rubocop to 1.62.1.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
-describe 'Homepage', js: true, type: :feature do
-  before :each do
+describe 'Homepage', :js, type: :feature do
+  before do
     visit '/'
   end
+
   it 'displays index.html page' do
     expect(page.find('h1')).to have_content 'Grape'
   end
